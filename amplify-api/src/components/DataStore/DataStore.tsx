122374f2e45ba68be2a8150484709a7e7b5d8d4a
Jose Munoz <mmbhigher@gmail.com>
2024-07-07 09:19:35 -0600

    Add missing changes

---
+++ amplify-api/src/components/DataStore/DataStore.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './DataStore.css';
+
+interface DataStoreProps {}
+
+const DataStore: FC<DataStoreProps> = () => (
+  <div className="DataStore">
+    DataStore Component
+  </div>
+);
+
+export default DataStore;
